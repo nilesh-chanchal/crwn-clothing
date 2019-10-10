ebd0d8c9ebf992e85c989c1db2d2fd7050e1e82b
nilesh-chanchal <nilesh.chanchal1004@gmail.com>
2019-10-10 23:45:53 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 
@@ -10,10 +11,12 @@ import ShopPage from './pages/shop/shop.component'
 function App() {
   return (
     <div>
+    <Header/>
     <Switch>
         <Route  exact path='/' component={HomePage} />
         <Route  exact path='/shop' component={ShopPage} />
      </Switch>
+     
     </div>
   );
 }
