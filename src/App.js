773cd7572c3c594f495e56c2a7b34b3c79f9fb75
nilesh-chanchal <nilesh.chanchal1004@gmail.com>
2019-10-11 20:45:55 +0530

    added sign in and sign out components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignOutPage from './pages/sign-in-sign-out/sign-in-sign-out.component';
 
 
 
@@ -13,8 +14,10 @@ function App() {
     <div>
     <Header/>
     <Switch>
+    
         <Route  exact path='/' component={HomePage} />
         <Route  exact path='/shop' component={ShopPage} />
+        <Route  exact path='/signin' component={SignInAndSignOutPage} />
      </Switch>
      
     </div>
